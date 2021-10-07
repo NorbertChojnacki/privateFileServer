e637494c0e80fcb58f5d3b9be904a59f4c3e59cc
Norbert Chojnacki <norbert.a.chojnacki@gmail.com>
2021-10-08 00:00:03 +0200

    reszta

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require("express")
 const http = require("http")
 const https = require("https")
 const expressSession = require("express-session")
+const path = require("path")
 require("dotenv").config()
+
+//* Routery
+const loginRouter = require("./routes/login")
+    app.use('/loginRegisterForms', loginRouter)
+
 
 const PORT = process.env.PORT || 80
 
 const app = express()
 
+app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname, "public")))
+
+
+
 app.get("/", (req, res)=>{
-    res.send("hello worlds")
+    res.render("index")
 })
 
-
+//* Opcje startu serwera
+//? W pliku .env trzeba zdecydowac czy ma byc http czy https
 if(process.env.HTTPS === "true"){
     console.log("https")
 }else{
