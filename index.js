49bb7ae431ab2c9bc7d603a4ac7645c8b587c67d
Norbert Chojnacki <norbert.a.chojnacki@gmail.com>
2021-10-07 22:39:22 +0200

    poczatek

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const cpus = require("os").cpus().length
+const cluster = require("cluster")
+
+require("dotenv").config()
+
+const PORT = process.env.PORT || 80
+
+if(cpus > 1){
+    if(cluster.isMaster){
+        for(let i=0; i<cpus; i++) cluster.fork()
+
+        cluster.on("exit", (worker)=>{
+            console.log(`Worker ${worker.id} has exitted.`)
+        })
+    }else{
+
+        console.log(`Express is listening on ${PORT} and workder ${process.pid}`)
+    }
+}
