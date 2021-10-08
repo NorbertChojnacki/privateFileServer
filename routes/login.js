9896a2b9a63570150c1a3134b9074aba1ce3db48
Norbert Chojnacki <norbert.a.chojnacki@gmail.com>
2021-10-08 17:26:14 +0200

    dzaialajace wysylanie fomrularzy przez fetch przy uzyciu formData

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,19 @@
 const express = require("express")
+const {body, validationResult} = require("express-validator")
 const router = express.Router()
+const multer = require("multer")
 
-router.post('/login', (req,res)=>{
+// router.post('/login', (req,res)=>{
 
-})
+// })
 
 
-router.post('/register', (req,res)=>{
-    
+router.post('/register', multer().none() ,(req,res)=>{
+
+    console.log(JSON.stringify(req.body))
+    res.status(400)
+    res.send("blablabla")
+    res.end()
 })
 
 module.exports = router
